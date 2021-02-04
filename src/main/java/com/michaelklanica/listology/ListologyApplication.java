f8c525532c925201bd4e95e035560a69e196b130
Michael Klanica <michael.david.klanica@gmail.com>
2021-02-04 17:10:59 -0600

    initial commit

---
+++ src/main/java/com/michaelklanica/listology/ListologyApplication.java
@@ -0,0 +1,13 @@
+package com.michaelklanica.listology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michaelklanica/listology/ListologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelklanica.listology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
